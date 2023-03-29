07643b409d53e81e02815c83e882516fdddb16a5
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-03-28 22:14:47 -0500

    edited JavaScript code for practice

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random()*topics.length)];
 
-function listTopics() {
+var listTopics = () => {
     for (var x = 0; x < topics.length; x++) {
       console.log(topics[x]);
-    }
   }
+}
 
-function selectTopic() {
-    if (randomTopic === 'HTML') {
+var selectTopic = () => {
+    /*if (randomTopic === 'HTML') {
     console.log("Let's study HTML!");
     } else if (randomTopic === 'CSS') {
     console.log("Let's study CSS!");
@@ -18,14 +18,15 @@ function selectTopic() {
     console.log("Let's study JavaScript!");
     } else {
     console.log('Please try again!');
-    }
+    }*/
+
+    return "Let's study " + randomTopic + "!";
 }
 
 console.log('Here are the topics we learned through Prework:');
-
 
 listTopics();
 
 console.log('Which topic should we study first?');
 
-selectTopic();
+console.log(selectTopic());
